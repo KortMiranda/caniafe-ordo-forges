6d62d276e328e69724b639acb6368d60e14f7822
kortmiranda <kortneymiranda@outlook.com>
2021-07-14 14:45:46 -0500

    add kit options on home page

--- caniafe-site/src/pages/Home.js
+++ caniafe-site/src/pages/Home.js
@@ -1,33 +1,41 @@
 import React from 'react';
-import { client } from '../client';
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import MainCarousel from '../components/MainCarousel'
-// import KitOptions from '../components/KitOptions'
-// import ReviewCarousel from '../components/ReviewCarousel'
+
 
 
 class Home extends React.Component {
-    // state = {
-    //     articles: []
-    // }
-
-    // componentDidMount(){
-    //     client.getEntries()
-    //     .then((response) => {
-    //         console.log(response)
-    //         this.setState({
-    //             articles:response.items
-    //         })
-    //     })
-    //     .catch(console.error)
-    // }
+ 
     render() {
         return (
             <div>
                 <MainCarousel />
-                {/* <KitOptions kits={this.state.articles} />
-                <ReviewCarousel reviews={this.state.articles} /> */}
-             </div>
+                <div className="kit-options">
+                    <div className="kit-option">
+                        <div className="kit-item">
+                            <h3 className="kit-title">Molded and Painted Kits</h3>
+                        </div>
+                        <div className="kit-item">
+                            <img src="https://res.cloudinary.com/dl3d3eyto/image/upload/v1624653928/Caniafe%20Ordo%20Forges%20Website/il_1588xN.3046129678_syqu_lzshfn.jpg" alt="Custom Mandalorian Armor" />
+                        </div>
+                        <div className="kit-item">
+                            <Link to="/store/painted-kits"><button>Explore Painted Armor</button></Link>
+                        </div>
+                    </div>
+                    <div className="kit-option">
+                        <div className="kit-item">
+                            <h3 className="kit-title">DIY Mandalorian Armor</h3>
+                        </div>
+                        <div className="kit-item">
+                            <img src="https://res.cloudinary.com/dl3d3eyto/image/upload/v1624654628/Caniafe%20Ordo%20Forges%20Website/il_1588xN.3090738657_7do5_xyjabo.jpg" alt="Custom Mandalorian Armor" />
+                        </div>
+                        <div className="kit-item">
+                            <Link to="/store/painted-kits"><button>Explore DIY Kits</button></Link>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
          );
     }
     
