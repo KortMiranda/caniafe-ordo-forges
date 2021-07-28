071e0ad0511f6bfaa4d894b3c66781577df3a94b
kortmiranda <kortneymiranda@outlook.com>
2021-07-28 14:09:18 -0500

    connected to python database

--- caniafe-site/src/pages/Store.js
+++ caniafe-site/src/pages/Store.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 
 function Store(props) {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        function getProduct() {
+            axios.get(`http://localhost:8000/products/`)
+            .then(res => {
+                setProducts(res.data)
+                console.log(res.data)
+            })
+            .catch(console.error)
+        }
+        getProduct()
+    }, [])
     return (
         <div>
             Store page
