203c0aa8b21e996f5319173878aaea4b63c9dca1
kortmiranda <kortneymiranda@outlook.com>
2021-08-07 17:45:15 -0500

    add product details page

--- caniafe-site/src/App.js
+++ caniafe-site/src/App.js
@@ -4,6 +4,7 @@ import Home  from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
 import Store from './pages/Store'
+import ProductDetails from './pages/ProductDetails'
 import CustomOrder from './pages/CustomOrder'
 import PaintedKits from './pages/PaintedKits'
 import DiyKits from './pages/DiyKits'
@@ -18,6 +19,7 @@ function App() {
       <Route exact path="/about" component={About} />
       <Route exact path="/services" component={Services} />
       <Route exact path="/store" component={Store} />
+      <Route exact path="/product/:id" component={ProductDetails} />
       <Route exact path="/store/painted-kits" component={PaintedKits} />
       <Route exact path="/store/diy-kits" component={DiyKits} />
       <Route exact path="/custom-order" component={CustomOrder} />
--- caniafe-site/src/pages/Store.js
+++ caniafe-site/src/pages/Store.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom'
+import Grid from "@material-ui/core/Grid";
 import axios from 'axios'
 
 function Store(props) {
@@ -9,15 +11,28 @@ function Store(props) {
             axios.get(`http://localhost:8000/products/`)
             .then(res => {
                 setProducts(res.data)
-                console.log(res.data)
             })
             .catch(console.error)
         }
         getProduct()
     }, [])
+
     return (
         <div>
-            Store page
+           <h2>Caniafe Ordo Forges Store</h2>
+           <Grid container className="product-grid">
+           {products.map((product) => {
+               return(
+                   <Grid item xs={8} sm={4} md={3} lg={2} xl={1} className="product-item">
+                            <Link to={`product/${product.id}`}>
+                            <img src={product.image} alt={product.name} />
+                            <p>{product.name}</p>
+                            <p>${product.price}</p>
+                            </Link>
+                        </Grid>
+                   )
+                })}
+            </Grid>
         </div>
     );
 }
